1f58377c5ee7f8d69d68a52666c3e2606e50e63d
Sylvain Mailler <sylvain.mailler@lmd.ipsl.fr>
2022-11-29 11:42:35 +0100

    Working bisection + test speed

--- fortran/FreeFall.F90
+++ fortran/FreeFall.F90
@@ -4,10 +4,10 @@ module FreeFall
 
   private
   
-  public :: delta_iter, iprec
+  public :: Re_iter, Re_bisection, Re_aersett, iprec
   
   integer, parameter :: iprec=4  ! 4 or 8
-
+  real(kind=iprec), parameter  :: eps=.5e-2
 
 contains
 
@@ -17,23 +17,58 @@ contains
     
     f_cliftgauvin = 0.2415*Re**0.687+Re/12.*0.42/(1+19018/(Re**1.16))
   end function f_cliftgauvin
+
+  function error(R, Re)
+    real(kind=iprec) ::error
+    real(kind=iprec), intent(in) :: r, Re
+
+    error = abs(Re-R/(1+f_cliftgauvin(Re)))/Re
+  end function error
+
+  function Re_aersett(R)
+    real(kind=iprec)  :: Re_aersett
+    real(kind=iprec), intent(IN)  :: R
+    
+    Re_aersett = R * (1 - (1+(R/2.440)**(-0.4335))**(-1.905))
   
-  function delta_iter(R)
+  end function Re_aersett
+
+  function Re_bisection(R)
+    real(kind=iprec)  :: Re_bisection
+    real(kind=iprec), intent(IN)  :: R
+    real(kind=iprec)  :: Re_lo, Re_hi, err
+    
+    Re_lo=0.
+    Re_hi=R
+    Re_bisection=R
+    err=error(R,Re_bisection)
+    do while(err>eps)
+       Re_bisection=0.5*(Re_hi+Re_lo)
+       if(R/(1+f_cliftgauvin(Re_bisection))<Re_bisection)then
+          Re_hi=Re_bisection
+       else
+          Re_lo=Re_bisection
+       endif
+       err=(Re_hi-Re_lo)/Re_lo
+    enddo
+    
+  end function Re_bisection
+  
+  function Re_iter(R)
     real(kind=iprec), intent(IN) :: R
-    real(kind=iprec) :: delta_iter
-    real(kind=iprec), parameter  :: eps=5.e-3
-    real(kind=iprec)             :: d, dprev, error
+    real(kind=iprec) :: Re_iter
+    real(kind=iprec)             :: d, dprev, err
     
   ! Evaluate the delta function as a function of the virtual Reynolds number Re
         d=0.
-        error=1.
-        do while(error>eps)
+        err=error(R,R)
+        do while(err>eps)
             dprev = d
             d = 1./(1+f_cliftgauvin(R*(1+d))) - 1.
-            error = abs(d-dprev)
-         enddo
-        delta_iter=d
-  end function delta_iter
+            err = abs(d-dprev)/(1+d)
+        enddo
+        Re_iter=(1+d)*R
+  end function Re_iter
     
   subroutine print_hello()
 
--- fortran/testspeed.F90
+++ fortran/testspeed.F90
@@ -1,14 +1,60 @@
 program testspeed
-use FreeFall, only : iprec, delta_iter
+use FreeFall, only : iprec, Re_iter, Re_bisection, Re_aersett
   
 implicit none
 
-real(kind=iprec) :: R
-integer      :: i
+real(kind=iprec) :: R, dumsum
+integer      :: i, j
+integer, parameter :: testsize=10000000
+real(kind=iprec), dimension(testsize) :: testvec
+real(kind=iprec), parameter :: min_exp=1., max_exp=4.
+integer :: count_ini, count, count_max
+real(kind=8) :: count_rate
+CALL SYSTEM_CLOCK(count, count_rate, count_max)
+WRITE(*,*) count, count_rate, count_max
 
-do i=1,1000
-   R=0.1*i
-   print*,i, delta_iter(R)
+do i=0,100
+   R = 10** (min_exp+(i/100.)*(max_exp-min_exp))
+   print*,i, R, Re_iter(R), Re_bisection(R), Re_aersett(R), &
+        & (Re_aersett(R)-Re_bisection(R))/Re_bisection(R)*100.,(Re_iter(R)-Re_bisection(R))/Re_bisection(R)*100.
 enddo
+
+! Generate a large random vector of R between 1 and 1.e5
+
+call random_number(testvec)
+testvec=min_exp+(max_exp-min_exp)*testvec
+testvec=10**testvec
+
+dumsum=0.
+CALL SYSTEM_CLOCK(count_ini, count_rate, count_max)
+! test speed of AerSett
+do i=1, testsize
+   dumsum=dumsum+Re_aersett(testvec(i))
+enddo
+CALL SYSTEM_CLOCK(count, count_rate, count_max)
+print*, 'Aersett execution time (s):', (count-count_ini)/count_rate
+print*,'dumsum',dumsum
+
+
+
+! test speed of iter
+dumsum=0.
+CALL SYSTEM_CLOCK(count_ini, count_rate, count_max)
+do i=1, testsize
+   dumsum=dumsum+Re_iter(testvec(i))
+enddo
+CALL SYSTEM_CLOCK(count, count_rate, count_max)
+print*, 'Iter execution time (s):', (count-count_ini)/count_rate
+print*,'dumsum',dumsum
+
+! test speed of bisection
+dumsum=0.
+CALL SYSTEM_CLOCK(count_ini, count_rate, count_max)
+do i=1, testsize
+   dumsum=dumsum+Re_bisection(testvec(i))
+enddo
+CALL SYSTEM_CLOCK(count, count_rate, count_max)
+print*, 'Bisection execution time (s):', (count-count_ini)/count_rate
+print*,'dumsum',dumsum
 
 end program testspeed
