eedc2b225a4bf008b87e4295f80417ee84749ba7
Sylvain Mailler <sylvain.mailler@lmd.ipsl.fr>
2022-12-08 15:13:30 +0100

    Adjust the epsilon value and produce stairs plot

--- fortran/plotspeed.py
+++ fortran/plotspeed.py
@@ -9,13 +9,13 @@ expintervals=np.asarray([0.,1.,2.,3.,4.])
 expcenters=10.**(np.asarray([.5, 1.5, 2.5, 3.5]))
 
 niters={}
-niters['fixpoint']=[ 3.51564956, 6.18300438, 9.87728405, 15.4518681 ]
-niters['bisection']=[ 6.83266497, 7.42684412, 8.51116753, 9.70776367 ]
+niters['fixpoint']=[ 3.40601826, 5.91496563, 9.50085926, 14.9136591 ]
+niters['bisection']=[ 6.24640656, 7.11500883, 8.24732399, 9.46562958 ]
 
 exetime={}
-exetime['fixpoint']=[ 103.2399999999999, 188.25999999999999, 309.86000000000001, 473.31000000000000 ]
-exetime['bisection']=[ 167.60000000000002, 184.69000000000000, 210.28000000000000, 226.96000000000001 ]
-exetime['aersett']=[ 18.149999999999999, 18.039999999999999, 18.410000000000000, 18.780000000000001]
+exetime['fixpoint']=[ 96.719999999999999, 177.25000000000000, 288.95999999999998, 455.00999999999999 ]
+exetime['bisection']=[ 151.72000000000000, 176.13000000000000, 198.63000000000000, 216.66999999999999 ]
+exetime['aersett']=[ 17.800000000000001, 18.109999999999999, 18.270000000000000, 18.260000000000002 ]
 
 labeldict={}
 labeldict['fixpoint']='Fixed-point method'
@@ -33,10 +33,10 @@ for method in ('aersett', 'bisection', 'fixpoint'):
 
 fig,ax=plt.subplots(figsize=(4,3))
 for method in ('aersett', 'bisection', 'fixpoint'):
-  plt.plot(expcenters, exetime[method], label=labeldict[method], color=colordict[method])
-  plt.plot([10**0.5, 10**3.5], [avgdict[method], avgdict[method]], color=colordict[method], linestyle=':', linewidth=1.)
+  plt.stairs(edges=10**expintervals, values=exetime[method], label=labeldict[method], color=colordict[method])
+  plt.plot([10**0., 10**4], [avgdict[method], avgdict[method]], color=colordict[method], linestyle=':', linewidth=1.)
   plt.text(10**3., avgdict[method]+4., "avg={:.1f} ns".format(avgdict[method]), color=colordict[method], fontsize='x-small')
-plt.legend()
+plt.legend(loc='upper left')
 plt.xscale('log')
 ax.set_xlim([1.,1.e+4])
 ax.set_ylim([0.,500.])
